01313324b21cd3e4982e1ca0bb43bc12622cb0a3
2529716798 <2529716798@qq.com>
2018-10-11 23:54:23 +0800

    bug unfixed

--- Pre-Functions/util/CSVUtil.py
+++ Pre-Functions/util/CSVUtil.py
@@ -1,4 +1,7 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+import os
 
 
 class CSVUtil:
@@ -16,7 +19,7 @@ class CSVUtil:
 
         col1, col2 = tf.decode_csv(value, record_defaults=self.record_defaults)
 
-        features = tf.stack([col1, col2,])
+        features = tf.stack([col1, col2])
 
         init_op = tf.global_variables_initializer()
         local_init_op = tf.local_variables_initializer()
@@ -41,4 +44,7 @@ class CSVUtil:
                 coord.request_stop()
                 coord.join(threads)
 
-reader = CSVUtil(['/Users/py/GitHub/Real-Output/dataset/sqrt_minus.csv'], [1., 1.])
+
+print(os.getcwd())
+csv_reader = CSVUtil(['sqrt_minus.csv'], [[1.], [1.]])
+csv_reader.read_csv()
